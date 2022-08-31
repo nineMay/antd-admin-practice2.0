640273377bebaeb18f7c2b5b885b05b090049fa7
wgp <3069934411@qq.com>
2022-08-31 17:23:10 +0800

    opt(): 添加警告

--- src/store/modules/ws.ts
+++ src/store/modules/ws.ts
@@ -1,9 +1,13 @@
+import { EVENT_KICK, EVENT_UPDATE_MENU } from "@/core/socket/event-type";
 import {
+  SocketIOWrapper,
   SocketIOWrapperType,
   SocketStatus,
   SocketStatusType,
 } from "@/core/socket/socket-io";
+import { Modal } from "ant-design-vue";
 import { defineStore } from "pinia";
+import { useUserStore } from "./user";
 
 interface WsState {
   client: SocketIOWrapperType | null;
@@ -27,6 +31,32 @@ export const useWsStore = defineStore({
         return;
       }
       this.status = status;
+    },
+    // 初始化Socket
+    initSocket() {
+      // 检查是否为初始化
+      if (this.client?.isConnected?.()) {
+        return;
+      }
+      const ws = new SocketIOWrapper();
+      ws.subscribe(EVENT_KICK, (data) => {
+        const userStore = useUserStore();
+        //重置token
+        userStore.resetToken();
+        Modal.warning({
+          title: "警告",
+          content: `您已被管理员${data.operater}踢下线`,
+          centered: true,
+          okText: "重新登录",
+          onOk() {
+            //刷新页面
+            window.location.reload();
+          },
+        });
+      });
+      ws.subscribe(EVENT_UPDATE_MENU, () => {
+        const userStore = useUserStore();
+      });
     },
   },
 });
